8b049ad06f729000409dc732db47b15d725783ff
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-07 15:14:29 +0200

    Initial commit

---
+++ spring_fumetteria/src/main/java/org/lessons/java/spring_fumetteria/SpringFumetteriaApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_fumetteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFumetteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFumetteriaApplication.class, args);
+	}
+
+}
