ad540f78a6a485208926612e1f88277ed913a310
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-07 15:43:04 +0200

    create Car.java in models folder

--- spring_fumetteria/src/main/java/org/lessons/java/spring_cars/models/Car.java
+++ spring_fumetteria/src/main/java/org/lessons/java/spring_cars/models/Car.java
@@ -1,10 +1,103 @@
 package org.lessons.java.spring_cars.models;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "cars")
 public class Car {
 
+    // variabili d'istanza
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(min = 5, max = 100, message = "car model field cannot be shorter than 5 chars and longer than 100")
+    @NotBlank(message = "car model field cannot be blank, empty or null")
+    private String model;
+
+    @NotNull(message = "production date field cannot be null")
+    private LocalDate productionDate;
+
+    @Size(min = 5, max = 20, message = "fuel type field cannot be shorter than 3 chars and longer than 20")
+    @NotBlank(message = "fuel type field cannot be blank, empty or null")
+    private String fuelType;
+
+    @NotNull(message = "number of doors field cannot be null")
+    private Integer numberOfDoors;
+
+    @NotNull(message = "price field cannot be null")
+    @Min(value = 0, message = "price field value cannot be less than 0")
+    private BigDecimal price;
+
+    @Lob
+    private String description;
+
+    // metodi
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public LocalDate getProductionDate() {
+        return this.productionDate;
+    }
+
+    public void setProductionDate(LocalDate productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public String getFuelType() {
+        return this.fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Integer getNumberOfDoors() {
+        return this.numberOfDoors;
+    }
+
+    public void setNumberOfDoors(Integer numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
