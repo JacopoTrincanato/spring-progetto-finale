afbff2e813bc334836cb72835c1a5f288a72efd6
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-08 11:57:54 +0200

    create User.java in models folder

--- spring_cars/src/main/java/org/lessons/java/spring_cars/models/Role.java
+++ spring_cars/src/main/java/org/lessons/java/spring_cars/models/Role.java
@@ -4,13 +4,17 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
