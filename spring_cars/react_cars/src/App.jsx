ea997bfa93847ff992ec031ea6b124d1de91a191
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-09 10:39:42 +0200

    add GlobalContext, BrowserRouter, Routes and Route elements in App.jsx

--- spring_cars/react_cars/src/App.jsx
+++ spring_cars/react_cars/src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import GlobalContext from './contexts/GlobalContext'
+import DefaultLayout from './layout/DefaultLayout';
 
 function App() {
 
+  //creo la costante per l'url dell'API
+  const apiUrl = import.meta.env.API_URL;
+
   return (
     <>
-
+      <GlobalContext.Provider value={{ apiUrl }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' />
+              <Route path='/optionals' />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
